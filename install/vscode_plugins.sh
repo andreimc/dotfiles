bba018a869416bc7f1a6ac4e8265c38543537cfb
Andrei Miulescu <lusu777@gmail.com>
2019-08-10 08:17:35 +1000

    Add vscode plugins

---
+++ install/vscode_plugins.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+code --install-extension vscodevim.vim
+code --install-extension ms-vscode.go
+code --install-extension dracula-theme.theme-dracula
+code --install-extension jakebecker.elixir-ls
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension editorconfig.editorconfig
+code --install-extension iampeterbanjo.elixirlinter
+code --install-extension eamodio.gitlens
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension mohsen1.react-javascript-to-typescript-transform-vscode
+code --install-extension wholroyd.jinja
+code --install-extension silvenon.mdx
+code --install-extension ms-vscode.node-debug2
+code --install-extension foxundermoon.shell-format
+code --install-extension mauve.terraform
+code --install-extension mjmcloug.vscode-elixir
+code --install-extension sammkj.vscode-elixir-formatter
+code --install-extension redhat.vscode-yaml
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension shan.code-settings-sync
+code --install-extension peterjausovec.vscode-docker
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension redhat.vscode-yaml
